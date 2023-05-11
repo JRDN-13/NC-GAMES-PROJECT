dde062bf9f7c51a3eb8623ecbab37c23852c5e26
JRDN-13 <ajordan183@gmail.com>
2023-05-11 13:47:42 +0100

    test data updated for flexibility

--- app.js
+++ app.js
@@ -20,4 +20,5 @@ app.use((err, req, res, next) => {
   }
 });
 
+
 module.exports = app;
--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -2,8 +2,8 @@ const { fetchApiData } = require("../models/api.model");
 
 exports.getApiData = (req, res, next) => {
   fetchApiData()
-    .then((data) => {
-      res.status(200).send({data});
+    .then((endpoints) => {
+      res.status(200).send({endpoints});
     })
     .catch(next);
 };
