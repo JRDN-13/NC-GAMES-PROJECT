769a34da3709d56350e282ba46dce3f7e8490a6a
JRDN-13 <ajordan183@gmail.com>
2023-05-11 14:09:09 +0100

    updated for task4 GET api reviews by ID

--- app.js
+++ app.js
@@ -11,14 +11,17 @@ app.get("/api/categories", getCategories);
 
 app.get("/api", getApiData);
 
+app.get("/api/reviews/:review_id", getReviewById);
+
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
+  } else if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request!" });
   } else {
     console.log(err);
     res.status(500).send({ msg: "Internal Server Error" });
   }
 });
-
 
 module.exports = app;
