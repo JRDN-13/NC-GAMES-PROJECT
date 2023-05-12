7afb08e94717276320c425028075b3df8f0b27a6
JRDN-13 <ajordan183@gmail.com>
2023-05-12 12:37:20 +0100

    files updated for hosting

--- db/connection.js
+++ db/connection.js
@@ -9,11 +9,12 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE not set');
 }
 
-const config = {}
-
-if(ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL
-  config.max = 2
-}
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
 
 module.exports = new Pool(config);
